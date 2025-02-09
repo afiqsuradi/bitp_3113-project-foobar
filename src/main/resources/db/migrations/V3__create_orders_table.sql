16b1f40c53a4a415caa92e7a596b5e3d73bdfd65
Aiman Afiq <aimanafiq05@gmail.com>
2025-02-09 17:49:52 +0800

    please drop ur table, im tired of handling migration

--- src/main/resources/db/migrations/V3__create_orders_table.sql
+++ src/main/resources/db/migrations/V3__create_orders_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     membership_id INT,
-    status ENUM('DONE', 'PROCESSING'),
+    status ENUM('DONE', 'PROCESSING', 'PENDING'),
     FOREIGN KEY (membership_id) REFERENCES memberships(id)
 );
